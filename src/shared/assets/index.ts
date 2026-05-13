c50fd6fe558060ed3a51e21300215d5098fd831d
cptblackmore <redshatterr@gmail.com>
2026-05-13 16:11:21 +0300

    feat(assets): add SVG icons and logo exports

---
+++ src/shared/assets/index.ts
@@ -0,0 +1,11 @@
+// Logos
+export { default as MagLogo } from './logos/mag-logo.svg?react'
+
+// Icons
+export { default as BellIcon } from './icons/bell.svg?react'
+export { default as CalendarIcon } from './icons/calendar.svg?react'
+export { default as DashboardIcon } from './icons/dashboard.svg?react'
+export { default as FolderIcon } from './icons/folder.svg?react'
+export { default as ListChecksIcon } from './icons/list-checks.svg?react'
+export { default as SettingsIcon } from './icons/settings.svg?react'
+export { default as SidebarIcon } from './icons/sidebar.svg?react'
