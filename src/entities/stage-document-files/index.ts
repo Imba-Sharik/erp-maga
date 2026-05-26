ad59fc8d2cdbd374c60ca4b1b42fe78982bd4fe3
cptblackmore <redshatterr@gmail.com>
2026-05-26 16:42:59 +0300

    refactor(stage-document-files): remove local zustand file cache

--- src/entities/stage-document-files/index.ts
+++ src/entities/stage-document-files/index.ts
@@ -1,3 +1,2 @@
 export type { StageDocumentType } from './model/types'
 export { documentFileKey } from './model/types'
-export { stageDocumentFilesActions, useStageDocumentFilesStore } from './model/store'
